e2a1b5d0fa19ca31a9cec8dc2e6b32a8403fa4c4
Diego <deidago7@hotmail.com>
2015-05-23 09:23:31 -0500

    V 2.0

--- src/uni/fiis/parte1/CalcularInteres.java
+++ src/uni/fiis/parte1/CalcularInteres.java
@@ -1,5 +1,16 @@
 package uni.fiis.parte1;
-
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.DoubleSummaryStatistics;
+/*Librerías para trabajar con archivos excel*/
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 public class CalcularInteres{
     private double prestamo;
     private int periodo;
@@ -64,19 +75,64 @@ public class CalcularInteres{
     }
 
     public static void main(String[] args) {
-        int i;
+        int i,f;
+        String cadena;
+        Workbook libro = new HSSFWorkbook();
+        Sheet hoja = libro.createSheet("new sheet");
         CalcularInteres joven= new CalcularInteres(15000,24,0.36);
         joven.setTasaMensual();
         System.out.println("Prestamos a 24 cuotas");
         System.out.println("# de Cuota\t Cuota por mes\tInteres\t    Amortizacion\tSaldo");
-        for(i=0;i<24;i++){
-            System.out.printf("     %d\t\t\t",i+1);
-            System.out.printf("%.2f\t\t",joven.pago_mensual());
-            System.out.printf("%.2f\t\t",joven.interes());
-            System.out.printf("%.2f\t\t\t",joven.amortizacion());
-            joven.setSaldo(joven.getSaldo() - joven.amortizacion());
-            System.out.printf("%.2f\n",joven.saldo);
+        for(i=0;i<25;i++){
+            Row fila = hoja.createRow(i);
 
+            Cell celda = fila.createCell(1);
+            if (i == 0) {
+                celda.setCellValue("# de Cuota");
+            } else {
+                cadena=Integer.toString(i);
+                celda.setCellValue(cadena);
+            }
+            celda = fila.createCell(2);
+            if (i == 0) {
+                celda.setCellValue("Cuota por mes");
+            } else {
+                cadena=Double.toString(joven.pago_mensual());
+                celda.setCellValue(cadena);
+            }
+            celda = fila.createCell(3);
+            if (i == 0) {
+                celda.setCellValue("Interes");
+            } else {
+                cadena=Double.toString(joven.interes());
+                celda.setCellValue(cadena);
+            }
+            celda = fila.createCell(4);
+            if (i == 0) {
+                celda.setCellValue("Amortizacion");
+            } else {
+                cadena=Double.toString(joven.amortizacion());
+                celda.setCellValue(cadena);
+            }
+
+            celda = fila.createCell(5);
+            if (i == 0) {
+                celda.setCellValue("Saldo");
+            } else {
+                joven.setSaldo(joven.getSaldo() - joven.amortizacion());
+                celda.setCellValue(joven.saldo);
+            }
         }
+        try {
+            FileOutputStream fileOut = new FileOutputStream("workbook.xls");
+            libro.write(fileOut);
+            fileOut.close();
+        }catch(FileNotFoundException e){
+            throw new RuntimeException();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
